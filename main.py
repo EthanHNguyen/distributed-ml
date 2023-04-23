3a5058ec3569d77a3e917e93546a44e63d1586bf
EthanHNguyen <ethanhuunguyen@gmail.com>
2023-04-23 13:11:30 -0500

    added simple mlp for mnist

--- main.py
+++ main.py
@@ -12,6 +12,19 @@ import os
 import random
 import numpy as np
 
+class MNIST_MLP(nn.Module):
+        def __init__(self, input_size):
+            super(MNIST_MLP, self).__init__()
+            self.fc1 = nn.Linear(input_size, 50)
+            self.relu = nn.ReLU()
+            self.fc2 = nn.Linear(50, 10)
+    
+        def forward(self, x):
+            out = self.fc1(x)
+            out = self.relu(out)
+            out = self.fc2(out)
+            return out
+
 def set_random_seeds(random_seed=0):
 
     torch.manual_seed(random_seed)
@@ -41,11 +54,10 @@ def evaluate(model, device, test_loader):
 def main():
 
     num_epochs_default = 10000
-    batch_size_default = 256 # 1024
-    learning_rate_default = 0.1
+    batch_size_default = 512 # 1024
+    learning_rate_default = 1e-2
     random_seed_default = 0
-    model_dir_default = "saved_models"
-    model_filename_default = "resnet_distributed.pth"
+    model_filename_default = "mnist.pth"
 
     # Each process runs on 1 GPU device specified by the local_rank argument.
     parser = argparse.ArgumentParser(formatter_class=argparse.ArgumentDefaultsHelpFormatter)
@@ -54,9 +66,9 @@ def main():
     parser.add_argument("--batch_size", type=int, help="Training batch size for one process.", default=batch_size_default)
     parser.add_argument("--learning_rate", type=float, help="Learning rate.", default=learning_rate_default)
     parser.add_argument("--random_seed", type=int, help="Random seed.", default=random_seed_default)
-    parser.add_argument("--model_dir", type=str, help="Directory for saving models.", default=model_dir_default)
+    # parser.add_argument("--model_dir", type=str, help="Directory for saving models.", default=model_dir_default)
     parser.add_argument("--model_filename", type=str, help="Model filename.", default=model_filename_default)
-    parser.add_argument("--resume", action="store_true", help="Resume training from saved checkpoint.")
+    # parser.add_argument("--resume", action="store_true", help="Resume training from saved checkpoint.")
     argv = parser.parse_args()
 
     local_rank = argv.local_rank
@@ -64,18 +76,9 @@ def main():
     batch_size = argv.batch_size
     learning_rate = argv.learning_rate
     random_seed = argv.random_seed
-    model_dir = argv.model_dir
+    # model_dir = argv.model_dir
     model_filename = argv.model_filename
-    resume = argv.resume
-
-    # Create directories outside the PyTorch program
-    # Do not create directory here because it is not multiprocess safe
-    '''
-    if not os.path.exists(model_dir):
-        os.makedirs(model_dir)
-    '''
-
-    model_filepath = os.path.join(model_dir, model_filename)
+    # resume = argv.resume
 
     # We need to use seeds to make sure that the models initialized in different processes are the same
     set_random_seeds(random_seed=random_seed)
@@ -85,7 +88,7 @@ def main():
     # torch.distributed.init_process_group(backend="gloo")
 
     # Encapsulate the model on the GPU assigned to the current process
-    model = torchvision.models.resnet18(pretrained=False)
+    model = MNIST_MLP(input_size=784)
 
     device = torch.device("cuda:{}".format(local_rank))
     model = model.to(device)
@@ -93,32 +96,30 @@ def main():
 
     # We only save the model who uses device "cuda:0"
     # To resume, the device for the saved model would also be "cuda:0"
-    if resume == True:
-        map_location = {"cuda:0": "cuda:{}".format(local_rank)}
-        ddp_model.load_state_dict(torch.load(model_filepath, map_location=map_location))
+    # if resume == True:
+    #     map_location = {"cuda:0": "cuda:{}".format(local_rank)}
+    #     ddp_model.load_state_dict(torch.load(model_filepath, map_location=map_location))
 
     # Prepare dataset and dataloader
     transform = transforms.Compose([
-        transforms.RandomCrop(32, padding=4),
-        transforms.RandomHorizontalFlip(),
         transforms.ToTensor(),
-        transforms.Normalize((0.4914, 0.4822, 0.4465), (0.2023, 0.1994, 0.2010)),
+        transforms.Normalize((0.1307,), (0.3081,))
     ])
 
     # Data should be prefetched
     # Download should be set to be False, because it is not multiprocess safe
-    train_set = torchvision.datasets.CIFAR10(root="data", train=True, download=False, transform=transform) 
-    test_set = torchvision.datasets.CIFAR10(root="data", train=False, download=False, transform=transform)
+    train_set = torchvision.datasets.MNIST(root="./data", train=True, download=True, transform=transform)
+    test_set = torchvision.datasets.MNIST(root="./data", train=False, download=True, transform=transform)
 
     # Restricts data loading to a subset of the dataset exclusive to the current process
     train_sampler = DistributedSampler(dataset=train_set)
 
-    train_loader = DataLoader(dataset=train_set, batch_size=batch_size, sampler=train_sampler, num_workers=8)
+    train_loader = DataLoader(dataset=train_set, batch_size=batch_size, sampler=train_sampler, num_workers=2)
     # Test loader does not have to follow distributed sampling strategy
-    test_loader = DataLoader(dataset=test_set, batch_size=128, shuffle=False, num_workers=8)
+    test_loader = DataLoader(dataset=test_set, batch_size=128, shuffle=False, num_workers=2)
 
     criterion = nn.CrossEntropyLoss()
-    optimizer = optim.SGD(ddp_model.parameters(), lr=learning_rate, momentum=0.9, weight_decay=1e-5)
+    optimizer = optim.ADAM(ddp_model.parameters(), lr=learning_rate)
 
     # Loop over the dataset multiple times
     for epoch in range(num_epochs):
@@ -129,7 +130,7 @@ def main():
         if epoch % 10 == 0:
             if local_rank == 0:
                 accuracy = evaluate(model=ddp_model, device=device, test_loader=test_loader)
-                torch.save(ddp_model.state_dict(), model_filepath)
+                torch.save(ddp_model.state_dict(), model_filename)
                 print("-" * 75)
                 print("Epoch: {}, Accuracy: {}".format(epoch, accuracy))
                 print("-" * 75)
